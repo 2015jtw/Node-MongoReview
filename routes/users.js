b0f5401e47c26fe8e37ca5494e7e102561582b9e
Josh <2015JTW@gmail.com>
2023-04-02 13:12:14 -0400

    feat: completed nodeJS section

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,24 @@ router.get('/', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
-    res.send("USER NEW FORM")
+    res.render('users/new')
 })
 
 router.post('/', (req, res) => {
+    const isValid = true;
+    if(isValid) {
+        users.push({firstName: req.body.firstName})
+        res.redirect(`/users/${users.length - 1}`)
+    }
+    else{
+        console.log('Invalid')
+        res.render('users/new', {firstName: req.body.firstName})
+    }
     res.send('Create User')
 })
 
 router.get('/:id', (req, res) => {
+    console.log(req.user)
     res.send(`Get User with id: ${req.params.id}`)
 })
 
@@ -25,7 +35,11 @@ router.delete('/:id', (req, res) => {
     res.send(`Delete User with id: ${req.params.id}`)
 })
 
-
+const users = [{name: 'John'}, {name: 'Jane'}]
+router.param('id', (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
+
+app.get('/', logger, (req, res) => {
     res.render('index', {
         text: 'world'
     })
@@ -12,6 +14,11 @@ app.get('/', (req, res) => {
 const userRouter = require('./routes/users');
 
 app.use("/users", userRouter)
+
+function logger(req, res, next){
+    console.log(req.originalUrl);
+    next();
+}
 
 app.listen(3000)
 
