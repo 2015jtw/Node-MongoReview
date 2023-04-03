43ddb7fda8cdde5ef98ca14dbe197379d561f48c
Josh Worden <2015JTW@gmail.com>
2023-04-03 10:09:46 -0400

    started using mongoose

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose');
+const User = require("./user")
+
 
 mongoose.connect("mongodb://localhost/testDB")
+
+run()
+async function run(){
+    const user = await User.create({name: 'josh', age: 26})
+    console.log(user);
+}
+
